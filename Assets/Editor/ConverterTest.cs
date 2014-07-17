97bcb0ecfc6bc2b0eb296f61c2e33b63e21fb340
Olga Cardenas <oh.cardenas@gmail.com>
2014-07-17 18:47:05 -0500

    RejectsInvalidFormat

---
+++ Assets/Editor/ConverterTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace UnityTest
+{
+	[TestFixture]
+	internal class ConverterTest
+	{
+		[Test]
+		public void CanBeCreated ()
+		{
+			Converter converter = new Converter ();
+			Assert.IsNotNull (converter);
+		}
+
+		[Test]
+		[ExpectedException]
+		public void RejectsInvalidFormat ()
+		{
+			Converter converter = new Converter ();
+			converter.ToBerlinFormat ("00-00-00");
+		}
+	}
+}
---
+++ Assets/Scripts/Converter.cs
@@ -0,0 +1,8 @@
+﻿
+public class Converter 
+{
+	public void ToBerlinFormat (string timeStr)
+	{
+		throw new System.ArgumentException ("Invalid Format");
+	}
+}
