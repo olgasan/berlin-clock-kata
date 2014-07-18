073f13aae7bfdc9dc173f55f98e8aa584b9c14cd
Olga Cardenas <oh.cardenas@gmail.com>
2014-07-17 19:04:08 -0500

    ReceivesTimeInFormat HH:MM:SS

--- Assets/Editor/ConverterTest.cs
+++ Assets/Editor/ConverterTest.cs
@@ -20,5 +20,20 @@ namespace UnityTest
 			Converter converter = new Converter ();
 			converter.ToBerlinFormat ("00-00-00");
 		}
+
+		[Test]
+		public void ReceivesTimeInFormatHHMMSS ()
+		{
+			Converter converter = new Converter ();
+
+			string h = "01";
+			string m = "02";
+			string s = "03";
+			string time = "{0}:{1}:{2}";
+			time = string.Format (time, h, m, s);
+
+			BerlinTime receivedTime = converter.ToBerlinFormat (time);
+			Assert.IsNotNull (receivedTime);
+		}
 	}
 }
--- Assets/Scripts/Converter.cs
+++ Assets/Scripts/Converter.cs
@@ -1,8 +1,47 @@
-﻿
-public class Converter 
+﻿public class Converter 
 {
-	public void ToBerlinFormat (string timeStr)
+	public class FormattedTime
 	{
-		throw new System.ArgumentException ("Invalid Format");
+		private string timeStr;
+
+		public FormattedTime (string timeStr)
+		{
+			this.timeStr = timeStr;
+		}
+
+		public bool IsValidFormat ()
+		{
+			string[] timeSegments = timeStr.Split (':');
+			return timeSegments.Length == 3;
+		}
+	}
+
+	public BerlinTime ToBerlinFormat (string timeStr)
+	{
+		bool isValid = ValidateTime (timeStr);
+
+		if (isValid)
+		{
+			return new BerlinTime ();
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	private bool ValidateTime (string timeStr)
+	{
+		bool isValid = true;
+		FormattedTime t = new FormattedTime (timeStr);
+
+		if (!t.IsValidFormat ())
+		{
+			isValid = false;
+			throw new System.ArgumentException ("Invalid Format");
+		}
+
+		return isValid;
 	}
 }
+
