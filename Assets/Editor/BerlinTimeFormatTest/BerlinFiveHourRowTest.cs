5e40104fbf6d0264a5df492fba774a86a9a3f014
Olga Cardenas <oh.cardenas@gmail.com>
2014-07-25 19:46:43 -0500

    Created BerlinFiveHourRow

---
+++ Assets/Editor/BerlinTimeFormatTest/BerlinFiveHourRowTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace UnityTest
+{
+	[TestFixture]
+	internal class BerlinFiveHourRowTest 
+	{
+		private BerlinFiveHourRow hourRow;
+
+		[SetUp]
+		public void SetUp ()
+		{
+			hourRow = new BerlinFiveHourRow ();
+		}
+
+		[Test]
+		public void FormatFiveHours ()
+		{
+			Assert.AreEqual ("ROOO", hourRow.ToBerlinFormat ("05"));
+		}
+
+		[Test]
+		public void FormatFifteenHours ()
+		{
+			Assert.AreEqual ("RRRO", hourRow.ToBerlinFormat ("15"));
+		}
+
+		[Test]
+		public void FormatTwentyThreeHours ()
+		{
+			Assert.AreEqual ("RRRR", hourRow.ToBerlinFormat ("23"));
+		}
+	}
+}
---
+++ Assets/Scripts/BerlinTimeFormat/BerlinFiveHourRow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BerlinFiveHourRow : BerlinRow 
+{
+	protected override int NumberOfBlocks 
+	{
+		get { return 4; }
+	}
+
+	protected override int NumberOfUnitsPerBlock 
+	{
+		get { return 5; }
+	}
+
+	public BerlinFiveHourRow ()
+	{
+		blockParser = new BlockParser ("R");
+	}
+
+	protected override bool ShouldBeEmpty (int value)
+	{
+		return !(value >= NumberOfUnitsPerBlock);
+	}
+
+	public override string ToBerlinFormat (string valueStr)
+	{
+		return ParseTimeRow (valueStr);
+	}
+}
